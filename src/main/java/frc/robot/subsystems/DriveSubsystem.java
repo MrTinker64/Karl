b13ac22b85a894f738aa0401bc1d5550f09eb64a
Avi Katzman <avi.micah@gmail.com>
2023-01-20 16:16:46 -0800

    Made victors private

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -14,10 +14,10 @@ import frc.robot.Constants.DriveConstants;
 
 public class DriveSubsystem extends SubsystemBase {
   // Define Victors
-  WPI_VictorSPX leftFront = new WPI_VictorSPX(0);
-  WPI_VictorSPX leftRear = new WPI_VictorSPX(1);
+  private WPI_VictorSPX leftFront = new WPI_VictorSPX(0);
+  private WPI_VictorSPX leftRear = new WPI_VictorSPX(1);
   // The motors on the left side of the drive.
-  MotorControllerGroup m_leftMotors = new MotorControllerGroup(leftFront, leftRear);
+  private final MotorControllerGroup m_leftMotors = new MotorControllerGroup(leftFront, leftRear);
   // The motors on the right side of the drive.
   private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(
       new PWMSparkMax(DriveConstants.kRightMotor1Port),
