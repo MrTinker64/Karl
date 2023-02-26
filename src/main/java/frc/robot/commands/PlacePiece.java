679bd5124dfc08744ef2b0cc0a2eb3826f733173
MrTinker64 <avi.micah@gmail.com>
2023-02-26 13:46:24 -0800

    Added a delay to the arm commands

--- src/main/java/frc/robot/commands/PlacePiece.java
+++ src/main/java/frc/robot/commands/PlacePiece.java
@@ -7,26 +7,18 @@ package frc.robot.commands;
 import frc.robot.subsystems.ArmSubsystem;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants;
 
-public class DefaultArm extends CommandBase {
+public class PlacePiece extends CommandBase {
     private final ArmSubsystem m_arm;
-    private final Trigger m_x;
-    private final Trigger m_a;
-    private final Trigger m_b;
-    private final Trigger m_y;
 
     /**
      * Creates a new DefaultDrive.
      *
      * @param subsystem The drive subsystem this command wil run on.
      */
-    public DefaultArm(ArmSubsystem subsystem, Trigger x, Trigger a, Trigger b, Trigger y) {
+    public PlacePiece(ArmSubsystem subsystem) {
         m_arm = subsystem;
-        m_x = x;
-        m_a = a;
-        m_b = b;
-        m_y = y;
         addRequirements(m_arm);
     }
 
@@ -38,18 +30,19 @@ public class DefaultArm extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        if (m_b.getAsBoolean()) {
-            m_arm.raiseArm();
-        } else if (m_x.getAsBoolean()) {
+
+        m_arm.raiseArm();
+        Timer.delay(1);
+
+        if (Constants.ArmConstants.lastGamePiece == Constants.ArmConstants.CONE) {
             m_arm.cubeInConeOut();
-        } else if (m_a.getAsBoolean()) {
+        } else if (Constants.ArmConstants.lastGamePiece == Constants.ArmConstants.CUBE) {
             m_arm.coneInCubeOut();
-        } else if (m_y.getAsBoolean()) {
-            m_arm.lowerArm();
         } else {
             m_arm.holdPiece();
-            m_arm.idleArm();
         }
+
+        m_arm.lowerArm();
         Timer.delay(1);
     }
 
@@ -61,6 +54,6 @@ public class DefaultArm extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return true;
     }
 }
